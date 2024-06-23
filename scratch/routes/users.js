40e47eab56cc36619a80e52b577f958182f1ff42
2003devansh <faujdardevansh@gmail.com>
2024-06-23 13:25:53 +0530

    commiting changes

--- scratch/routes/users.js
+++ scratch/routes/users.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const mongoose  = require("mongoose") ;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/scratch") ; 
+
+const userSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  cart: {
+    type: Array,
+    default: []
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  orders:{
+    type: Array  , 
+  },
+  contact: {
+   type : Number
+  },
+  picture: {
+    type: String,
+   
+  }
 });
 
-module.exports = router;
+module.exports  = mongoose.model("user" ,userSchema) ; 
+
