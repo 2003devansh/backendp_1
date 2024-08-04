2e41d9cc0603837bfc5d2791f98b7bf20b081c81
2003devansh <faujdardevansh@gmail.com>
2024-08-04 13:38:52 +0530

    k

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,12 +6,16 @@ console.log(process.env.NODE_ENV);
 
 
 router.get("/" , function(req,res){
-    res.send("hey");
+    res.send("heyoi");
 }) ;
 
 if(process.env.NODE_ENV === "development"){
-    router.post("/create" , (req,res)=>{
-        res.send("heyy its working")
+    router.post("/create" , async (req,res)=>{
+       let owners  = await ownerModel.find() ;
+       if(owners.length > 0 ){
+        return res.send(503).send("ypu don't have the authority ")
+       }
+       res.send("we can create owner")
     })
 }
 // jab tak hamara node ka envioment developpmentmai set hai tab tal yeh route chalega
